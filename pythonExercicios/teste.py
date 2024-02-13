ed4b807a844b889620f8ab2d0b41d1f9db128e3a
Eduardo Farias Rizk <Dudurizk@gmail.com>
2024-02-12 22:05:15 -0300

    Update teste.py

--- pythonExercicios/teste.py
+++ pythonExercicios/teste.py
@@ -1,18 +1,48 @@
-lista = []
-for c in range (1,6):
-    valor = int(input("Digite um novo valor: "))
-    if c == 1 or valor > lista[-1]:
-        lista.append(valor)
+lista  = []
+contP = 0
+
+while True:
+    pessoas = []
+    nome = input("Qual o primeiro nome: ")
+    contP += 1
+    pessoas.append(nome)
+    peso = int(input("Qual o peso: "))
+    pessoas.append(peso)
+    lista.append(pessoas[:]) #por enquanto eu só peguei os dados e copiei para uma lista 
+    
+    decisao  = input("Voçê quer continuar adicionando pessoas? ").upper().strip()
+    if decisao in "N":
+        break
+#Agora eu quero pegar os valores das pessoas mais pesadas e leves    
+pesoMenor = pesoMaior = 0
+nomeMenor = [] #Nome das pessoas que pesam meno kilos
+nomeMaior = []
+contador = 0
+while contador < len(lista):
+    if contador == 0:
+        pesoMaior = lista[contador][1]
+        pesoMenor = lista[contador][1]
+        nomeMenor = [lista[contador][0]]
+        nomeMaior = [lista[contador][0]]
     else:
-        x = 0
-        while x < len(lista):
-            if valor < lista[x]:
-                lista.insert(x,valor)
-                break
-            x =+ 1
-print(lista)
-
-
+        if pesoMenor > lista[contador][1]:
+            pesoMenor = lista[contador][1]
+            nomeMenor = [lista[contador][0]]
+        elif pesoMenor == lista[contador][1]:
+            nomeMenor.append(lista[contador][0])
+        
+        if pesoMaior < lista[contador][1]:
+            pesoMaior = lista[contador][1]
+            nomeMaior = [lista[contador][0]]
+        elif pesoMaior == lista[contador][1]:
+            nomeMaior.append(lista[contador][0])
+    contador += 1
+        
+print(f"A lista teve {contP} cadastradas, além disso essas foram as pessoas mais pesadas {nomeMaior}")
+print(f"E essas foram com o menor peso {nomeMenor}")            
+        
+    
+    
 
 
 

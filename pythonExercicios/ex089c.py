00eeea36a307da645021a3b9ffe0192baa6fd51d
Eduardo Farias Rizk <eduardo.rizk@sou.inteli.edu.br>
2024-03-23 21:35:40 -0300

    alt

---
+++ pythonExercicios/ex089c.py
@@ -0,0 +1,38 @@
+total = []
+temp = [] #vai guardar o nome, a lista com as duas notas, além da média
+notas = []
+
+while True:
+    temp = [] #vai guardar o nome, a lista com as duas notas, além da média
+    notas = []
+    nome = input("Qual o nome do aluno: ")
+    temp.append(nome)
+    #pegando as notas e guardando em uma lista
+    nota1 = int(input("Qual a primeira nota: "))
+    notas.append(nota1)
+    nota2 = int(input("Qual a sua segunda nota: "))
+    notas.append(nota2)
+    
+    temp.append(notas[:])
+    
+    media = (nota1 + nota2)/2
+    temp.append(media)
+    total.append(temp[:])
+    temp.clear() 
+    resp = " "
+    while resp not in "NnSs":
+        resp = input("Voçê deseja continuar? [S] [N]: ").upper().strip()[0]
+    if resp == "N":
+        # Break out of the loop if the user chooses not to continue
+        break
+print(total)
+
+print("Boletim:")
+for idx, aluno in enumerate(total):
+    print(f"Aluno {idx}: {aluno[0]}, Média: {aluno[2]}")
+escolha = int(input("Qual aluno voçê gostaria que fosse mostrado: "))
+for idx, aluno in enumerate(total):
+    if idx == escolha:
+        print("As notas foram: ")
+        for c in total[idx][1]:
+            print(f"A nota é: {c}")
--- pythonExercicios/ex090.py
+++ pythonExercicios/ex090.py
@@ -1,12 +1,9 @@
 dic = {}
-nomeDele = input("Qual o nome do aluno: ")
-dic["nome"] = nomeDele
-media  = float(input("Qual a media do aluno: "))
-dic["media"] = media
-for c,v in dic.items():
-    print(f"A {c} dele é {v}")
-
-if dic["media"] < 6:
-    print("Reprovado")
-else:
-    print("Aprovado")
+dic['nome'] = input("Qual o nome: ")
+dic['media'] = float(input("Qual a média do aluno: "))
+if dic['media'] >= 6:
+    dic['situacao'] = "Aprovado"
+elif dic['media'] < 6:
+    dic['situacao'] = 'Reprovado'
+for k,v in dic.items():
+    print(f"{k} é igual a {v}")
